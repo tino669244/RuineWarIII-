7b53c98c32a23aae8ccc661bd668e62459b154c4
Tino Ruine <tinoruine@gmail.com>
2025-10-04 12:00:26 +0300

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,1 +1,18 @@
-
+document.addEventListener('keydown',(e)=>{
+  switch(e.key){
+    case 'w': console.log('Move forward'); break;
+    case 'a': console.log('Move left'); break;
+    case 's': console.log('Move back'); break;
+    case 'd': console.log('Move right'); break;
+    case 'ArrowUp': console.log('Aim up'); break;
+    case 'ArrowDown': console.log('Aim down'); break;
+    case 'ArrowLeft': console.log('Aim left'); break;
+    case 'ArrowRight': console.log('Aim right'); break;
+    case ' ': console.log('Jump'); break;
+    case 'Shift': console.log('Sprint'); break;
+    case 'Control': console.log('Crouch'); break;
+    case 'b': console.log('Defend'); break;
+    case 't': console.log('Fire'); break;
+    case 'e': console.log('Interact'); break;
+  }
+});
