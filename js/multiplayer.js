6b5befada440be28661ef1e4fe8678360a849e46
Tino Ruine <tinoruine@gmail.com>
2025-10-04 11:59:03 +0300

    Update multiplayer.js

--- js/multiplayer.js
+++ js/multiplayer.js
@@ -1,1 +1,3 @@
-
+// Placeholder for future multiplayer functionality
+// Functions: connectToServer(), syncPlayerPositions(), broadcastActions()
+console.log("Multiplayer module ready for future implementation");
